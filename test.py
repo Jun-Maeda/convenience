3b0d7f884fc51da5bac91a67fbca6b508630a31d
maeda <jun_pure@icloud.com>
2020-01-18 10:17:22 +0900

    特定データのみ別ファイルで出力

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 #icvs.txtを取得して速報と確定のみ取り込む
 import datetime
 
-path = 'icvs.txt'
+path = 'icvs'
 f = open(path,  "r")
-
+douton_num = "15921"
 line = f.readline()
 
 sokuho = []
 kakutei = []
+douton = []
 while line:
-    if line[:4] == "2012":
+    if line[44:49] == douton_num:
+        douton.append(line)
+    elif line[:4] == "2012":
         sokuho.append(line)
     elif line[:4] == "2022":
         kakutei.append(line)
@@ -21,33 +24,51 @@ dt_now = datetime.datetime.now()
 today = str(dt_now.year) + str(dt_now.month) + str(dt_now.day)
 sokuhoname = "sokuho" + today + ".txt"
 kakuteiname = "kakutei" + today + ".txt"
+doutonname = "douton" + today + ".txt"
+
+with open(doutonname,mode="a") as f:
+    print("道頓堀\n",file=f)
+    for i in douton:
+        day = i[5:11]
+        num = i[44:49]
+        money = i[56:62]
+        come = i[80:88]
+        if i[:4] == "2012":
+            print("速報データ",file=f)
+            print("入金日：" + day,file=f)
+            print("顧客番号：" + num,file=f)
+            print("金額：" + money + "円\n\n",file=f)
+        else:
+            print("確定データ",file=f)
+            print("入金日：" + day,file=f)
+            print("顧客番号：" + num,file=f)
+            print("金額：" + money + "円",file=f)
+            print("振込日：" + come + "\n\n",file=f)
+
 
 with open(sokuhoname,mode="a") as f:
     print("速報データ\n",file=f)
     for i in sokuho:
         day = i[5:11]
-        num = i[42:48]
+        num = i[44:49]
         money = i[56:62]
         come = i[80:88]
         print("入金日：" + day,file=f)
         print("顧客番号：" + num,file=f)
         print("金額：" + money + "円\n\n",file=f)
-    
-    
+
+
 
 with open(kakuteiname,mode="a") as f:
     print("確定データ\n",file=f)
     for i in kakutei:
         day = i[5:11]
-        num = i[42:48]
+        num = i[44:49]
         money = i[56:62]
         come = i[80:88]
-    
-    
+
+
         print("入金日：" + day,file=f)
         print("顧客番号：" + num,file=f)
         print("金額：" + money + "円",file=f)
         print("振込日：" + come + "\n\n",file=f)
-        
-        
-        
